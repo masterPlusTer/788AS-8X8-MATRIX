b19f942542be86c85f40b4fb1f1bf999027ac24b
+Ter <7396589+masterPlusTer@users.noreply.github.com>
2025-01-14 15:41:17 -0400

    Actualizar bitmap.ino

--- bitmap.ino
+++ bitmap.ino
@@ -3,7 +3,7 @@
 ESTE ES EL PROMPR QUE LE DI A CHATGPT PARA QUE DEVUELVA ESTE CODIGO 
 
 
-tengo una matriz de 8x8 (788AS) conectada al arduino nano, te voy a dar las conecciones y tu dame un codigo sencillo para mostrar imagenes en la matriz,  pines 9, 14, 8, 12, 1, 7, 2 , y 5 de la matriz, son negativos y son las filas y van conectados respectivamente al d2, d3, d4, d5, d6, d7, d8 , y d9, del nano, estan conectados en este orden que te di van de orden acendente de fila 1 a fila 8, por favor no los confuncas, estos son los catodos. Los positivos son los pines 13, 3, 4, 10,6 11, 15, 16, son los anodos y estan conectados respectivamente al nano a los pines d10, d11, d12, a0 , a1, a2, a3, y a4 estos son las columnas, y y van en orden acendente de columna 1 a columna 8.
+tengo una matriz de 8x8 (788AS) conectada al arduino, te voy a dar las conecciones y tu dame un codigo sencillo para mostrar imagenes en la matriz,  pines 9, 14, 8, 12, 1, 7, 2 , y 5 de la matriz, son negativos y son las filas y van conectados respectivamente al d2, d3, d4, d5, d6, d7, d8 , y d9, del nano, estan conectados en este orden que te di van de orden acendente de fila 1 a fila 8, por favor no los confuncas, estos son los catodos. Los positivos son los pines 13, 3, 4, 10,6 11, 15, 16, son los anodos y estan conectados respectivamente al nano a los pines d10, d11, d12, a0 , a1, a2, a3, y a4 estos son las columnas, y y van en orden acendente de columna 1 a columna 8.
 
 
 */
